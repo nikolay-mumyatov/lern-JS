fd359d5697b906d83f432ba63abe0aac7b951918
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-05-31 18:16:58 +0300

    Выполнено ДЗ 3-его урока.

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,38 @@ addExpenses = addExpenses.toLowerCase();
 console.log(addExpenses.split(', '));
 
 let budgetDay = money / 30;
-console.log(budgetDay);
+console.log(budgetDay.toFixed(2)); // Дневной бюджет
+
+// - lesson_03 -
+
+money = prompt("Ваш ежемесячный доход?");
+addExpenses = prompt(
+  "Перечислите возможные расходы за рассчитываемый период через запятую"
+);
+deposit = confirm("Есть ли у вас депозит в банке?");
+
+let mandatoryExpensesOne = prompt('Введите обязательную статью расходов.'),
+    costExpensesOne = prompt('Во сколько это обойдется?'),
+    mandatoryExpensestwo = prompt('Введите обязательную статью расходов.'),
+    costExpensesTwo = prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - costExpensesOne - costExpensesTwo;
+console.log(`Бюджет на месяц: ${budgetMonth}`);
+
+let missionMonth = mission / budgetMonth;
+console.log(`${Math.ceil(missionMonth)} месяцев до достижения цели.`);
+
+budgetDay = budgetMonth / 30;
+console.log(`Ежедневный бюджет: ${Math.floor(budgetDay)}`); // Дневной бюджет
+
+if (budgetDay >= 1200){
+  console.log("У вас высокий уровень дохода");
+} else if (budgetDay >= 600 && budgetDay < 1199) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay <= 599 && budgetDay >= 0) {
+  console.log("К сожалению у вас уровень дохода ниже среднего");
+} else {
+  console.log('Что то пошло не так');
+}
+
+// жку, автомобиль, интернет, телефон, развлечения;
