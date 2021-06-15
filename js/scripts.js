8efb8d93fa4e390be4db70cff53ee4e0ed577262
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-15 17:39:28 +0300

    Задание второй части к 16-му уроку

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 'use strict';
 
-class First {
-  hello() {
-    console.log("Привет я метод родителя!");
-  }
-}
+/* Напишите функцию на JS. Цель: Убрать все объекты с типом additional, а для basic очки уменьшить в двое.
 
-class Second extends First {
-  hello() {
-    super.hello();
-    console.log("А я наследуемый метод!");
-  }
-}
+Изменить необходимо исходный массив*/
 
-const first = new First();
-const second = new Second();
-
-second.hello();
+const myLesson = [
+  {lesson: 1, type: 'basic', points: 2},
+  {lesson: 2, type: 'additional', points: 4},
+  {lesson: 3, type: 'basic', points: 6},
+  {lesson: 4, type: 'additional', points: 3},
+  {lesson: 5, type: 'basic', points: 4},
+  {lesson: 6, type: 'basic', points: 2},
+  {lesson: 7, type: 'additional', points: 2},
+  {lesson: 8, type: 'basic', points: 6},
+  {lesson: 9, type: 'basic', points: 4},
+  {lesson: 10, type: 'basic', points: 6},
+  {lesson: 11, type: 'additional', points: 5}, 
+  {lesson: 12, type: 'basic', points: 2}, 
+  {lesson: 13, type: 'additional', points: 2}, 
+  {lesson: 14, type: 'basic', points: 4},
+  {lesson: 15, type: 'additional', points: 1},
+  {lesson: 16, type: 'additional', points: 7},
+];
