7cf5909542e9ab68eeda41d04c5231d30477ad1b
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-17 22:11:54 +0300

    Выполнил сложное задание к уроку 18

--- js/scripts.js
+++ js/scripts.js
@@ -112,4 +112,20 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   };
   togglePopup();
+
+  //Плавный скролл страницы
+  const linksScroll = document.querySelectorAll("a");
+
+  for (let item of linksScroll) {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      let _this = this;
+
+      const linkHref = _this.getAttribute("href");
+      document.querySelector(linkHref).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    });
+  }
 });
