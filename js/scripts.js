ee99da3f0ddeeb4a139a1f065448149351c98bde
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-23 19:59:30 +0300

    Выполнил задание к уроку 22

--- js/scripts.js
+++ js/scripts.js
@@ -149,6 +149,114 @@ window.addEventListener("DOMContentLoaded", function () {
 
   tabs();
 
+  // Добавление точек в слайдер
+  const addDot = () => {
+    const slide = document.querySelectorAll('.portfolio-item'),
+          dotsBox = document.querySelector('.portfolio-dots');
+
+          slide.forEach((item, i) => {
+            let dot = document.createElement('li');
+            dot.classList.add('dot');
+
+            if(i === 0) {
+              dot.classList.add('dot-active');
+            }
+
+            dotsBox.appendChild(dot);
+          });
+
+  };
+  addDot();
+
+  // Слайдер
+  const slider = () => {
+    const slide = document.querySelectorAll('.portfolio-item'),
+          btn = document.querySelectorAll('.portfolio-btn'),
+          dot = document.querySelectorAll('.dot'),
+          slider = document.querySelector('.portfolio-content');
+
+    let currentSlide = 0,
+        interval;
+
+    const prevSlide = (elem, index, strClass) => {
+      elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+      elem[index].classList.add(strClass);
+    };
+
+    const autoPlaySlide = () => {
+      prevSlide(slide, currentSlide, 'portfolio-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+      currentSlide++;
+      if(currentSlide >= slide.length) {
+        currentSlide = 0;
+      }
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(dot, currentSlide,'dot-active');
+    };
+
+    const startSlide = (time = 3000) => {
+      interval = setInterval(autoPlaySlide, time);
+    };
+
+    const stopSlide = () => {
+      clearInterval(interval);
+    };
+
+    slider.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      let target = event.target;
+
+      if(!target.matches('.portfolio-btn, .dot')){
+        return;
+      }
+
+      prevSlide(slide, currentSlide, 'portfolio-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+      if(target.matches('#arrow-right')){
+        currentSlide++;
+      } else if (target.matches('#arrow-left')) {
+        currentSlide--;
+      } else if (target.matches('.dot')) {
+        dot.forEach((elem, index) => {
+          if(elem === target) {
+            currentSlide = index;
+          }
+        });
+      }
+
+      if(currentSlide >= slide.length) {
+        currentSlide = 0;
+      }
+
+      if(currentSlide < 0) {
+        currentSlide = slide.length - 1;
+      }
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(slide, currentSlide, 'dot-active');
+
+    });
+
+    slider.addEventListener('mouseover', (event) => {
+      if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+        stopSlide();
+      }
+    });
+
+    slider.addEventListener('mouseout', (event) => {
+      if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+        startSlide();
+      }
+    });
+
+    startSlide(3000);
+
+  };
+  slider();
+
   //Плавный скролл страницы
   const linksScroll = document.querySelectorAll("a");
 
