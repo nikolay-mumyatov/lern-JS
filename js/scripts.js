5fc8ca9fd93a8fadbdd77048c631df8c6dd0d29b
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-24 21:02:33 +0300

    Выполнено задание к уроку 23

--- js/scripts.js
+++ js/scripts.js
@@ -257,6 +257,23 @@ window.addEventListener("DOMContentLoaded", function () {
   };
   slider();
 
+  //Поменять картинку при наведении в блоке Наша команда.
+  const teamBox = document.querySelector('.team'), 
+        imgTeam = teamBox.querySelectorAll('.command__photo');
+  
+  imgTeam.forEach((item) => {
+    let srcAttr = item.getAttribute('src');
+    if(item.hasAttribute('data-img')) {
+      item.addEventListener('mouseenter', (event) => {
+        
+        item.src = event.target.dataset.img;
+      });
+      item.addEventListener('mouseleave', (event) => {
+        item.src = srcAttr;
+      });
+    }
+  });
+
   //Плавный скролл страницы
   const linksScroll = document.querySelectorAll("a");
 
@@ -272,4 +289,51 @@ window.addEventListener("DOMContentLoaded", function () {
       });
     });
   }
+});
+
+
+// Проверка инпутов
+const inputCalc = document.querySelectorAll('.number');
+inputCalc.forEach((elem) => {
+    elem.addEventListener("blur", () => {
+      elem.value = elem.value.replace(/\D/g, '');
+      elem.value = elem.value.trim();
+      elem.value = elem.value.replace(/^-+|-+$/g, '');
+  });
+});
+
+
+const inputWord = document.querySelectorAll('.input-text');
+inputWord.forEach((item) => {
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[^а-яА-ёЁ -]/g, '');
+      item.value = item.value.trim();
+      item.value = item.value.replace(/-{2,}/g,'-');
+      item.value = item.value.replace(/[ ]{2,}/g,' ');
+      item.value = item.value.replace(/^-+|-+$/g, '');
+      if (item.classList.contains('bio')){
+        item.value = item.value.replace(/( |^)[а-яё]/g, function(x){ return x.toUpperCase(); });
+      }
+
+  });
+});
+
+
+const inputEmail = document.querySelectorAll('.input-email');
+inputEmail.forEach((item) => {
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[^a-zA@\-_.!~*']/g, '');
+      item.value = item.value.trim();
+      item.value = item.value.replace(/^-+|-+$/g, '');
+  });
+});
+
+
+const inputPhone = document.querySelectorAll('.input-phone');
+inputPhone.forEach((item) => {
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[^1-9()0/-]/g, '');
+      item.value = item.value.trim();
+      item.value = item.value.replace(/^-+|-+$/g, '');
+  });
 });
