3d403bb4f7a994866330ad5a84e5b91cd67ba03a
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-07-14 20:55:01 +0300

    Добавлено аккордион и модальное окно

--- js/scripts.js
+++ js/scripts.js
@@ -60,27 +60,26 @@ const lowScroll = () => {
 
 lowScroll();
 
-
 const accordeon = () => {
   const accordeon = document.querySelector(".accordeon"),
-    tab = accordeon.querySelectorAll(".element"),
-    tabContent = document.querySelectorAll(".element-content");
+    tab = accordeon.querySelectorAll(".accordeon-element"),
+    tabContent = document.querySelectorAll(".accordeon-element-content");
 
   const toggleTab = (index) => {
     for (let i = 0; i < tabContent.length; i++) {
       if (index === i) {
         tab[i].classList.add("active");
-        tabContent[i].style.display = 'block';
+        tabContent[i].classList.remove("hidden-content");
       } else {
         tab[i].classList.remove("active");
-        tabContent[i].style.display = 'none';
+        tabContent[i].classList.add("hidden-content");
       }
     }
   };
 
   accordeon.addEventListener("click", (event) => {
     let target = event.target;
-    target = target.closest(".element");
+    target = target.closest(".accordeon-element");
 
     if (target) {
       tab.forEach((item, i) => {
