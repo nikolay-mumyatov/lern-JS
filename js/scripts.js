5ae8f7d4140eed8378d7e950a8518ea75a76f757
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-07-15 22:19:10 +0300

    Добавлена валидация и отправка на сервер

--- js/scripts.js
+++ js/scripts.js
@@ -151,3 +151,104 @@ const carusel = () => {
   });
 };
 carusel();
+
+
+
+
+const validateInput = () => {
+  const inputName = document.querySelectorAll(".form-name");
+  inputName.forEach((item) => {
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[^а-яА-ёЁ -]/g, "");
+      item.value = item.value.trim();
+      item.value = item.value.replace(/-{2,}/g, "-");
+      item.value = item.value.replace(/[ ]{2,}/g, " ");
+      item.value = item.value.replace(/^-+|-+$/g, "");
+      if (item.classList.contains("bio")) {
+        item.value = item.value.replace(/( |^)[а-яё]/g, function (x) {
+          return x.toUpperCase();
+        });
+      }
+    });
+  });
+  
+  const inputPhone = document.querySelectorAll(".input-phone");
+  inputPhone.forEach((item) => {
+    item.addEventListener("blur", () => {
+      item.value = item.value.replace(/[^1-9()0/+]/g, "");
+      item.value = item.value.trim();
+    });
+  });
+};
+validateInput();
+
+
+
+
+
+
+
+
+
+const sendForm = () => {
+  const errorMessage = "Ошибка! Письмо не отправлено.",
+    loadMessage = "Идет загрузка...",
+    successMessage = "Спасибо! Мы скоро с вами свяжемся!",
+    modalBlock = document.querySelector(".modal-callback");
+
+  const statusMessage = document.createElement("div");
+  statusMessage.style.cssText = "font-size: 16px";
+
+  const postData = (body) => {
+    return fetch("../server.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
+  const clearInput = () => {
+    const inputItem = document.querySelectorAll("input");
+    inputItem.forEach((item) => {
+      item.value = "";
+    });
+  };
+
+  const formAll = document.querySelectorAll(".form-ajax");
+
+  formAll.forEach((item) => {
+
+    item.addEventListener("submit", (event) => {
+      event.preventDefault();
+      item.appendChild(statusMessage);
+      statusMessage.style.color = "#000000";
+      const formData = new FormData(item);
+      let body = {};
+      formData.forEach((val, key) => {
+        body[key] = val;
+      });
+
+      statusMessage.textContent = loadMessage;
+
+      postData(body)
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error(response);
+          } else {
+            setTimeout(function () {
+              statusMessage.textContent = "";
+            }, 3000);
+            statusMessage.textContent = successMessage;
+            setTimeout(function () {
+              modalBlock.style.display = "none";
+            }, 3000);
+          }
+        })
+        .catch((error) => (statusMessage.textContent = errorMessage))
+        .finally(clearInput());
+    });
+  });
+};
+sendForm();
