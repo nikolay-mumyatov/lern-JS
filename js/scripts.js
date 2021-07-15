7d92346efb3de10ed51c31ae062bd2fc695030ca
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-07-15 21:26:41 +0300

    Добавлена карусель

--- js/scripts.js
+++ js/scripts.js
@@ -96,9 +96,6 @@ const accordeon = () => {
 };
 accordeon();
 
-
-
-
 const slider = () => {
   const slide = document.querySelectorAll(".item");
 
@@ -129,3 +126,28 @@ const slider = () => {
   startSlide(3000);
 };
 slider();
+
+const carusel = () => {
+  const carousel = document.querySelector(".services-carousel"),
+        itemCarousel = document.querySelectorAll(".carousel-item"),
+        arrowRight = document.querySelector(".arrow-right"),
+        arrowLeft = document.querySelector(".arrow-left");
+
+  let widthItem = 260,
+      count = 2,
+      positionStart = 0;
+
+  arrowRight.addEventListener("click", () => {
+    positionStart -= widthItem * count;
+    positionStart = Math.max(positionStart, -widthItem * (itemCarousel.length - count)
+    );
+    carousel.style.marginLeft = positionStart + "px";
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    positionStart += widthItem * count;
+    positionStart = Math.min(positionStart, 0);
+    carousel.style.marginLeft = positionStart + "px";
+  });
+};
+carusel();
